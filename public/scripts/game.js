9d875819e0db34c9e75fa794554d6d87c14784fb
Thanapat <devpatrick.cho@gmail.com>
2024-04-23 21:00:35 +0000

    feat: coin and blocker

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,8 +1,126 @@
 let gameState = {
   chickenLane: 0,
+  carScreenTime: 1500,
+  level: 'easy'
 }
 
+const objectState = {
+  chicken: null,
+  pipes: [],
+  lanes: [],
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  objectState.chicken = document.getElementById("chicken");
+  objectState.pipes = document.querySelectorAll(`[game-class="pipe"]`);
+  objectState.lanes = document.querySelectorAll(`[game-class="lane"]`);
+  moveChickenLane();
+  setActiveLane(1);
+});
+
 function moveChickenLane() {
-  gameState.chickenLane = (gameState.chickenLane + 1)
-  console.log(gameState.chickenLane)
+  const pipe = document.querySelector(`[lane="${gameState.chickenLane}"] [game-class="pipe"]`);
+  if (!pipe) return;
+  if (gameState.chickenLane > 1) {
+    const pipePosition = pipe.getBoundingClientRect();
+    const chickenPosition = objectState.chicken.getBoundingClientRect();
+    const distance = pipePosition.left - chickenPosition.left;
+    objectState.chicken.style.transform = `translateX(${distance}px)`;
+  } else if (gameState.chickenLane === 1) objectState.chicken.style.transform = "translateX(0px)";
+
+  if (gameState.chickenLane > 1) setTimeout(() => {
+    objectState.chicken.style.transition = "none";
+    objectState.chicken.style.transform = "translateX(0px)";
+    objectState.chicken.parentNode.removeChild(objectState.chicken);
+    pipe.parentNode.appendChild(objectState.chicken);
+    objectState.chicken.style.transition = "transform cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.3s";
+  }, 300)
+
+  setTimeout(() => setDead(false), 400);
+
+  setActiveLane(gameState.chickenLane + 1);
+  transformXGameView()
+}
+
+function moveToNextPipe() {
+  gameState.chickenLane += 1;
+  moveChickenLane();
+}
+
+function genCar(lane, numCar) {
+  const car = document.querySelector(`[lane="${lane}"] [game-class="car-wrapper"]`);
+  car.getBoundingClientRect();
+
+  const newCarImage = document.createElement('img');
+  newCarImage.setAttribute('src', `/assets/cars/car${numCar}.svg`);
+  newCarImage.setAttribute('game-class', 'car');
+  car.appendChild(newCarImage);
+  newCarImage.animate(
+    [
+      { transform: `translateY(${-car.getBoundingClientRect().height}px)` },
+      { transform: `translateY(${car.getBoundingClientRect().height}px)` },
+    ], {
+    duration: gameState.carScreenTime,
+    iterations: 1,
+    fill: 'forwards',
+  }
+  );
+  setTimeout(() => {
+    newCarImage.remove();
+  }, gameState.carScreenTime);
+}
+
+function setDead(dead) {
+  const lane = document.querySelector(`div.item[game-class="lane"][lane="${gameState.chickenLane}"]`);
+  lane.querySelector(`[game-class="pipe"]`).style.display = "none";
+  lane.querySelector(`[game-class="mul-text"]`).style.display = "none";
+  if (dead == true) {
+    //genCar
+
+  } else {
+    if (lane) {
+      const blockerCrack = document.createElement('img');
+      blockerCrack.setAttribute('src', '/assets/images/roadBlock-crack.svg');
+      blockerCrack.setAttribute('game-class', 'blocker-crack');
+      lane.appendChild(blockerCrack);
+      const blocker = document.createElement('img');
+      blocker.setAttribute('src', '/assets/images/roadBlock.svg');
+      blocker.setAttribute('game-class', 'blocker');
+      lane.appendChild(blocker);
+      blockerCrack.animate(
+        [
+          { transform: 'translateY(-55px)' },
+          { transform: 'translateY(-60px)' },
+        ], {
+        duration: 200,
+        delay: 100,
+        iterations: 1,
+        fill: 'forwards',
+      }
+      );
+      blocker.animate(
+        [
+          { transform: `translateY(-${lane.getBoundingClientRect().y}px)` },
+          { transform: 'translateY(-70px)' },
+        ], {
+        duration: 200,
+        iterations: 1,
+        fill: 'forwards',
+      }
+      );
+      if (gameState.chickenLane - 1 > 0) {
+        // let coin = `
+        //   <img class="coin" src="/assets/images/coin.svg" />
+        //   `
+        // lane.innerHTML += coin;
+        const previousLane = document.querySelector(`div.item[game-class="lane"][lane="${gameState.chickenLane - 1}"]`);
+        const coin = document.createElement('img');
+        coin.setAttribute('src', '/assets/images/coin.svg');
+        coin.setAttribute('game-class', 'coin');
+        coin.classList.add('coin');
+        previousLane.appendChild(coin);
+      }
+    }
+
+  }
 }
--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -11,3 +11,65 @@ function handleBetInput(event) {
     console.log("Enter pressed");
   }
 }
+
+
+function mouseOverPipe() {
+  this.style.opacity = 1;
+}
+
+function mouseLeavePipe() {
+  this.style.opacity = 0.5;
+}
+
+function setActiveLane(number) {
+  for (let i = 1; i <= 15; i++) {
+    const pipe = document.querySelector(`[lane="${i}"] [game-class="pipe"]`);
+    const mulText = document.querySelector(`[lane="${i}"] [game-class="mul-text"]`);
+    if (i === number) {
+      pipe.setAttribute('active', 'true');
+      pipe.addEventListener("click", moveToNextPipe);
+      mulText.setAttribute('active', 'true');
+      mulText.addEventListener("click", moveToNextPipe);
+    } else {
+      pipe.removeAttribute('active');
+      pipe.removeEventListener("click", moveToNextPipe);
+      mulText.removeAttribute('active');
+      mulText.removeEventListener("click", moveToNextPipe);
+    }
+  }
+}
+
+
+function adjustViewTransform(amount) {
+  const x = getComputedStyle(document.getElementById("view"))
+  const old = x.getPropertyValue("--viewTransform");
+  document.getElementById("view").style.setProperty("--viewTransform", `${parseFloat(old.split("px")[0] || 0) + amount}px`);
+}
+
+function setViewTransform(amount) {
+  document.getElementById("view").style.setProperty("--viewTransform", `${amount}px`);
+}
+
+function getViewTransform() {
+  const x = getComputedStyle(document.getElementById("view"))
+  return parseFloat(x.getPropertyValue("--viewTransform").split("px")[0] || 0);
+}
+
+function transformXGameView() {
+  const chicken = document.getElementById("chicken")
+  if (!chicken) return;
+  const gameContainer = document.getElementById("game-container");
+  const gameView = document.getElementById("view");
+  setTimeout(() => {
+    if (chicken.getBoundingClientRect().x >= gameContainer.getBoundingClientRect().width / 4) {
+      adjustViewTransform(-150 - Math.max(0, chicken.getBoundingClientRect().x - gameContainer.getBoundingClientRect().width / 4 - 150));
+      setViewTransform(Math.min(0, getViewTransform()));
+      gameView.style.transform = `translateX(${Math.max(gameContainer.getBoundingClientRect().width - gameView.getBoundingClientRect().width, getViewTransform())}px)`;
+    } else if (chicken.getBoundingClientRect().x < 0) {
+      console.log("refine to", chicken.getBoundingClientRect().x)
+      adjustViewTransform(-chicken.getBoundingClientRect().x);
+      setViewTransform(Math.min(0, getViewTransform()));
+      gameView.style.transform = `translateX(${Math.max(gameContainer.getBoundingClientRect().width - gameView.getBoundingClientRect().width, getViewTransform())}px)`;
+    }
+  }, 350) // 300ms is the time it takes for the chicken to move to the next lane + 50 ms for refinement / js delay
+}
